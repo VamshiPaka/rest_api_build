f3218a22ce6eba43276febd7855d934dc5b212be
VamshiPaka <vamshi.paka@gmail.com>
2021-05-24 10:32:48 +0530

    added manifest.yml and .vscodee

--- app.js
+++ app.js
@@ -1,37 +1,31 @@
-const express=require('express');
-const mongoose=require('mongoose');
-const bodyParser=require('body-parser');
-const cors=require('cors');
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 require('dotenv/config');
 
 const app = express();
 
 //import Routes
-
-const productsRoute=require('./routes/products');
-
-
-
+const productsRoute = require('./routes/products');
 //connect to db
 
-mongoose.connect(process.env.DB_Connection,{ useNewUrlParser: true , useUnifiedTopology: true },()=>console.log('connected to DB'));
-
+mongoose.connect(process.env.DB_Connection, { useNewUrlParser: true, useUnifiedTopology: true }, () =>
+	console.log('connected to DB')
+);
 
 //Middlewares(functions that are executed when a specific route is hit)
 
 app.use(bodyParser.json());
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use('/products',productsRoute);
+app.use('/products', productsRoute);
+app.use(express.json());
 
 //Routes
-app.get('/',(req,res)=>{
-    res.send('This is home route');
-})
-
-
-
-
+app.get('/', (req, res) => {
+	res.send('This is home route');
+});
 
 //Listening on port to start the server
 
--- routes/products.js
+++ routes/products.js
@@ -1,68 +1,68 @@
-const express=require('express');
+const express = require('express');
 const router = express.Router();
 
-const Product=require('../models/product');
+const Product = require('../models/product');
 
 // gets all the list of the products
-router.get('/',async(req,res)=>{
-    try{
-        const products=await Product.find();
-        res.json(products); 
-    }
-    catch(err){
-        res.json({message:err})
-    }
+router.get('/', async (req, res) => {
+	try {
+		// res.send('list of products');
+		const products = await Product.find();
+		res.json(products);
+	} catch (err) {
+		res.json({ message: err });
+	}
 });
-
-
 //to get a single product
-router.get('/:productid',async(req,res)=>{
-    try{
-    const product=await Product.findById(req.params.productid);
-    res.json(product)
-    }catch(err){
-        res.json({message:err});
-    }
+router.get('/:productid', async (req, res) => {
+	try {
+		const product = await Product.findById(req.params.productid);
+		res.json(product);
+	} catch (err) {
+		res.json({ message: err });
+	}
 });
 
 //adds a product
-router.post('/',async(req,res)=>{
-    const product=new Product({
-        product_name:req.body.product_name,
-        product_desc:req.body.product_desc,
-        product_price:req.body.product_price,
-        product_category:req.body.product_category
-    })
-    //save to database
-    try{
-    const savedProduct=await product.save()
-    res.json(savedProduct);
-    }catch(err){
-        res.json({message:err})
-    }
-
+router.post('/', async (req, res) => {
+	const product = new Product({
+		product_name: req.body.product_name,
+		product_desc: req.body.product_desc,
+		product_price: req.body.product_price,
+		product_category: req.body.product_category
+	});
+	//save to database
+	try {
+		const savedProduct = await product.save();
+		res.json(savedProduct);
+	} catch (err) {
+		res.json({ message: err });
+	}
 });
 
 //delete a specific post
 
-router.delete('/:productid',async (req,res)=>{
-    try{
-        const removeProduct = await Product.remove({_id:req.params.productid})
-        res.json(removeProduct);
-    }catch(err){
-        res.json({messsage:err})
-    }
-})
+router.delete('/:productid', async (req, res) => {
+	try {
+		const removeProduct = await Product.remove({ _id: req.params.productid });
+		res.json(removeProduct);
+	} catch (err) {
+		res.json({ messsage: err });
+	}
+});
 
 //Update a Product
 
-router.patch('/:productid',async (req,res)=>{
-    try{
-    const updatedPost = await Product.updateOne({_id:req.params.productid},{$set:{product_name:req.body.product_name}})
-    res.json(updatedPost);
-    }catch(err){
-        res.json({message:err})
-    }
-})
+router.patch('/:productid', async (req, res) => {
+	try {
+		const updatedPost = await Product.updateOne(
+			{ _id: req.params.productid },
+			{ $set: { product_name: req.body.product_name } }
+		);
+		res.json(updatedPost);
+	} catch (err) {
+		res.json({ message: err });
+	}
+});
 
-module.exports=router;
+module.exports = router;
